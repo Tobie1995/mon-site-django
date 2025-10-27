d93542dda3aacbb06fc83b1f495802b9f9bc6bc2
Tobie <tobie.m@h2altitude.com>
2025-10-27 12:52:58 +0100

    Remove unused Django project files and add requirements.txt and .gitignore

--- mon_site/mon_site/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from django.urls import path
-from hello import views as hello_views
-    
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', hello_views.bonjour, name='bonjour'),
-]
