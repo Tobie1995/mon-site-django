bd84722675608033d3c59a872a4a100e1ed0da2f
Tobie <tobie.m@h2altitude.com>
2025-10-28 12:23:43 +0100

    Feat: Ajout ModelForm pour Tache et tests unitaires

--- monsite/accueil/models.py
+++ monsite/accueil/models.py
@@ -1,12 +1,27 @@
 from django.db import models
 
-
-# Modèle représentant une tâche simple
 class Tache(models.Model):
+	"""Représente une tâche (to-do) simple.
+
+	Cette classe modèle stocke les informations minimales pour une tâche :
+	son titre et son état d'achèvement.
+
+	Attributes:
+		titre (str): Le titre ou la description courte de la tâche (max 200 caractères).
+		termine (bool): True si la tâche est complétée, False sinon. Valeur par défaut : False.
+
+	La méthode __str__ renvoie le titre pour une représentation lisible dans l'admin
+	et les interfaces Django.
+	"""
 
 	titre = models.CharField(max_length=200)
 	termine = models.BooleanField(default=False)
 
 	def __str__(self):
+		"""Retourne une représentation lisible de la tâche.
+
+		Returns:
+			str: Le titre de la tâche.
+		"""
 		return self.titre
 
